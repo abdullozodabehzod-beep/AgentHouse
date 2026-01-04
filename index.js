a088595a3a3fd8faaa3d397b775272e9b56c45c3
Behzod <abdullozodabehzodgmail.com>
2026-01-05 01:21:56 +0300

    Building 1

---
+++ index.js
@@ -0,0 +1,10 @@
+//-------------------------------------------------BURGER
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
+
+burger.onclick = () => {
+    nav.classList.toggle('active')
+}
+
+//-------------------------------------------------
+
